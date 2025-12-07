9952b2f76eb56ba32104c9c94acd112e0c823f56
Supakorn1998xi <supakorn0019@gmail.com>
2025-12-07 12:58:54 +0700

    init

--- dashboard.py
+++ dashboard.py
@@ -8,3 +8,78 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample SuperStore EDA")
+st.markdown(
+"""
+    <style>
+    div.block-container{
+        padding-top: 4rem;
+    }
+    </style>
+""",unsafe_allow_html=True
+)
+
+fl = st.file_uploader(":file_folder: upload a file",type=(["csv","txt","xlsx","xls"]))
+if fl is not None :
+    filename=fl.name
+    st.write(filename)
+    df=pd.read_csv(filename,encoding="ISO-8859-1")
+else :
+    os.chdir(r"C:\Users\Pimkunlaya.pim\Documents\Code\mulidashboard\mulidashboard")
+    df=pd.read_csv("Superstore.csv",encoding="ISO-8859-1")
+
+col1,col2=st.columns((2))
+df["Order Date"]=pd.to_datetime(df["Order Date"])
+
+## getting the min and max date
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate= pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date",startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date",endDate))
+
+df = df[(df["Order Date"]>=date1)&(df["Order Date"]<date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+
+#Create for region
+region = st.sidebar.multiselect("Pick your Region",df["Region"].unique())
+if not region:
+    df2=df.copy()
+else : 
+    df2=df[df["Region"].isin(region)]
+
+#Create for State
+state = st.sidebar.multiselect("Pick the State",df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3=df2[df2["State"].isin(state)]
+
+#Create for city
+city = st.sidebar.multiselect("Pick the city",df3["City"].unique())
+
+#Filter the data based on Region, State And City
+if not region and not state and not city:
+    filtered_df = df 
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)]
+elif state and city:
+    filtered_df = df3[df["State"].isin(state)&(df3["City"].isin(city))]
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region)&(df3["City"].isin(city))]
+elif region and state:
+    filtered_df = df3[df["Region"].isin(region)&(df3["State"].isin(state))]
+elif city :
+    filtered_df = df3[df3["City"].isin(city)]
+else:
+    filtered_df = df3[df3["Region"].isin(region)&(df3["State"].isin(state))&(df3["City"].isin(city))]
+
+
+
+
+
